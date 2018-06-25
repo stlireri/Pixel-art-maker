905ada37773e44fde18683ab97e4a950d46cd73c
stlireri <stellaireri@gmail.com>
2018-06-26 00:17:01 +0300

    add table heights, width and define the makeGdrid function

--- designs.js
+++ designs.js
@@ -2,9 +2,30 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+$(document).ready(function(){
+  $("#sizePicker").submit(function(event){
+    event.preventDefault();
 
-function makeGrid() {
+    row, column, color;
 
+    row = $("#inputWidth").val();
+
+    column = $("#inputHeight").val();
+
+    color = $("#colorPicker").val();
+    
+    makeGrid(row, column);
+  });
+});
+
+function makeGrid(height, width) {
+$("tr").remove()
 // Your code goes here!
+for(var x = 1; x <= height; x++){
+  $("#pixelCanvas").append("<tr></tr>");
+  for(var y = 1; y <= width; y++){
+    $("tr").filter(".last").append("<td></td>")
+  }
+};
 
 }
